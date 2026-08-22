64037d7d9700b02a006f448af08db8ddb70c9c22
raidjalouali <raidjalouali@yahoo.fr>
2026-08-22 23:59:04 +0200

    ci: add linter fixes and complete github actions pipeline

--- app/app.py
+++ app/app.py
@@ -4,12 +4,14 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return jsonify({
         "status": "online",
         "service": "api"
     })
+
 
 @app.route('/db-check')
 def db_check():
@@ -20,10 +22,12 @@ def db_check():
             password=os.getenv("MYSQL_PASSWORD", "secretpass"),
             database=os.getenv("MYSQL_DATABASE", "appdb")
         )
+        conn.close()
         return jsonify({"database": "connected", "status": 200})
     except Exception as e:
         return jsonify({"database": "error", "message": str(e)}), 500
 
+
 if __name__ == '__main__':
-    # 0.0.0.0 est nécessaire pour écouter sur toutes les interfaces du conteneur
+    # 0.0.0.0 est necessaire pour ecouter sur toutes les interfaces
     app.run(host='0.0.0.0', port=5000)
